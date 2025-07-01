b9944b19f95ce1500ecb43f4ceab75de34823782
Pulse Connect <freeadinath@gmail.com>
2025-07-01 15:17:14 +0530

    DUMMY

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,24 @@
-import React from 'react'
-import Dashboard from './pages/Dashboard'
-import Sidebar from './components/Sidebar'
-import Navbar from './components/Navbar'
-import './App.css'
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import Sidebar from "./components/Sidebar";
+import Navbar from "./components/Navbar";
+import "./App.css";
 
 function App() {
- 
   return (
-    <div className="flex flex-col min-h-screen">
-      <Navbar />
-      {/* Main content: Sidebar + Dashboard */}
-      <div className=" flex flex-1 w-full ">
-        <Sidebar />
-        <Dashboard />
+    <Router>
+      <div className="flex flex-col min-h-screen">
+        <Navbar />
+        <div className="flex flex-1 w-full">
+          <Sidebar />
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+          </Routes>
+        </div>
       </div>
-    </div>
+    </Router>
   );
-
 }
 
-export default App
+export default App;
